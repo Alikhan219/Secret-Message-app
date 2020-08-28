6c0c20c7f567b7d42c750dfe9bef4905a27acead
Alikhan219 <merabhai786786@gmail.com>
2020-08-28 16:32:21 +0500

    adding the windows.location and hiding the first page when user clicks on creat button and showing the link page

--- message.js
+++ message.js
@@ -1,7 +1,13 @@
 document.querySelector('form').addEventListener('submit', event =>{
+    document.querySelector('#message-form').classList.add('hide');
+    document.querySelector('#link-form').classList.remove('hide');
     event.preventDefault();
     const input= document.querySelector('#message-input');
    const encrypted= btoa(input.value);
-   document.querySelector('#link-input').value= encrypted;
-})
+
+
+   const linkInput =document.querySelector('#link-input');
+   linkInput.value= `${window.location}#${encrypted}`;
+   linkInput.select()
+});
 
